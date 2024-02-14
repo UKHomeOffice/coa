e0ece672bc7c5d64179238e1acf59c535c2ac8d1
adityababumallisettiHO <Aditya.Mallisetti@digital.homeoffice.gov.uk>
2024-02-14 15:34:19 +0000

    TLF-50: Setup CI pipeline
    * Add CI pipeline using lmr

---
+++ apps/lmr/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+'use strict';
+
+const SummaryPageBehaviour = require('hof').components.summary;
+
+module.exports = {
+  name: 'coa',
+  steps: {
+    '/property-address': {
+      fields: ['address-line-1', 'address-line-2', 'town-or-city', 'county', 'postcode'],
+      backLink: true,
+      next: '/summary'
+    },
+    '/summary': {
+      behaviours: [SummaryPageBehaviour],
+      template: 'confirm',
+      sections: require('./sections/summary-data-sections')
+    }
+  }
+};
