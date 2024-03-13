3dc847d7d3e62037aedc61fc58a48deb0a1bf2ad
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2024-03-13 18:09:47 +0000

    add dependants details page

--- apps/coa/index.js
+++ apps/coa/index.js
@@ -1,18 +1,51 @@
+const hof = require('hof');
+const Summary = hof.components.summary;
+const Aggregate = require('./behaviours/aggregator');
+const setDateErrorLink = require('./behaviours/set-date-error-link');
+const ModifyChangeURL = require('./behaviours/modify-change-link');
+
 module.exports = {
   name: 'coa',
   baseUrl: '/',
+  params: '/:action?/:id?/:edit?',
   steps: {
-    '/overview': {
-      next: '/what-you-need'
+    '/update-dependant': {
+      fields: ['change-dependant-details'],
+      forks: [{
+        target: '/dependant-details',
+        condition: {
+          field: 'change-dependant-details',
+          value: 'yes'
+        }
+      }],
+      next: '/confirm'
     },
-    '/what-you-need': {
-      next: '/proof-of-identity'
+    '/dependant-details': {
+      fields: [
+        'dependant-full-name',
+        'dependant-date-of-birth',
+        'dependant-country-of-nationality'
+      ],
+      behaviours: [setDateErrorLink],
+      next: '/dependant-summary'
     },
-    '/proof-of-identity': {
-      next: '/proof-of-address'
+    '/dependant-summary': {
+      behaviours: [Aggregate, hof.components.homeOfficeCountries],
+      aggregateTo: 'dependants',
+      aggregateFrom: [
+        'dependant-full-name',
+        'dependant-date-of-birth',
+        'dependant-country-of-nationality'
+      ],
+      addStep: 'dependant-details',
+      template: 'dependant-summary',
+      addAnotherLinkText: 'dependant',
+      next: '/confirm'
     },
-    '/proof-of-address': {
-      next: '/update-details'
+    // Change this to /check-answers
+    '/confirm': {
+      behaviours: [Summary, ModifyChangeURL],
+      sections: require('./sections/summary-data-sections')
     }
-  }
+  },
 };
