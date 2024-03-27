532367be5f647911e8c05783f5a8050c25bf9d41
RobertMcCann <79094247+RobertMcCann@users.noreply.github.com>
2024-03-27 11:53:27 +0000

    TLF-56-dependants details (#7)
    * add dependants details page
    
    * override items table partial to add missing govuk-link class to edit/delete buttons; change confirm step to check-answers
    
    * add styling for dependant summary table to shift edit/delete buttons to the right of the dependant's summary table
    
    * add dependant button to translations; add form flow so user can navigate to dependants details page

--- apps/coa/fields/index.js
+++ apps/coa/fields/index.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const moment = require('moment');
+const config = require('../../../config');
 const dateComponent = require('hof').components.date;
 const countries = require('hof').utils.countries();
 
@@ -23,6 +26,50 @@ function excludeUK(value) {
 }
 
 module.exports = {
+  
+  'change-dependant-details': {
+    isPageHeading: 'true',
+    mixin: 'radio-group',
+    validate: 'required',
+    className: ['block', 'form-group'],
+    options: [
+      'yes',
+      'no'
+    ]
+  },
+
+  'dependant-full-name': {
+    validate: ['required', 'notUrl', { type: 'maxlength', arguments: 200 }],
+    labelClassName: 'govuk-label--s'
+  },
+
+  'dependant-date-of-birth': dateComponent('dependant-date-of-birth', {
+    mixin: 'input-date',
+    validate: [
+      'required',
+      'date',
+      { type: 'before', arguments: ['0', 'days'] },
+      { type: 'after', arguments: ['120', 'years'] }
+    ],
+    parse: d => d && moment(d).format(config.PRETTY_DATE_FORMAT),
+    legend: {
+      className: 'govuk-label--s'
+    }
+  }),
+
+  'dependant-country-of-nationality': {
+    mixin: 'select',
+    className: ['typeahead'],
+    validate: [
+      'required',
+      excludeUK
+    ],
+    options: [{
+      value: '',
+      label: 'fields.dependant-country-of-nationality.options.none'
+    }].concat(_.sortBy(countries, o => o.label)),
+    labelClassName: 'govuk-label--s'
+  },
 
   'applicant-full-name': {
     mixin: 'input-text',
