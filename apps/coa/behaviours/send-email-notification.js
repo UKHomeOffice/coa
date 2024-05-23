0259c683ca287b81da026feb29a2644fd6944da1
Mike Maratheftis <mikemaratheftis@m-dqqh0q1n2p.home>
2024-05-23 10:51:38 +0100

    progress on TLF-106

--- apps/coa/behaviours/send-email-notification.js
+++ apps/coa/behaviours/send-email-notification.js
@@ -12,7 +12,11 @@ const notifyKey = config.govukNotify.notifyApiKey;
 const notifyClient = new NotifyClient(notifyKey);
 
 module.exports = class SendEmailConfirmation {
+  // getUpdatedDetails(value) {
+  //   const getUpdatedDetails = req.sessionModel.get('which-details-updating');
+  // }
   async sendUserEmailNotification(req) {
+    const detailsUpdating = this.getUpdatedDetails(req);
     const personalisations = this.notifyPersonalisations;
 
     try {
@@ -22,25 +26,91 @@ module.exports = class SendEmailConfirmation {
 
       await notifyClient.sendEmail(userConfirmationTemplateId, req.sessionModel.get('email'), {
         personalisation: Object.assign({}, personalisations, {
+        reference_number: req.sessionModel.get('uniqueRefNumber'),
+        applying_for_whom: req.sessionModel.get('isApplicant') ? 'your' : req.sessionModel.get('nameWithPossession'),
+        applicant_full_name: req.sessionModel.get('applicant-full-name'),
+        applicant_dob: req.sessionModel.get('applicant-dob'),
+        nationality: req.sessionModel.get('applicant-nationality'),
+        has_applicant_unique_number: req.sessionModel.get('applicant-unique-number').length > 0 ? 'yes' : 'no',
+        applicant_unique_number: req.sessionModel.get('applicant-unique-number'),
+        has_passport_number: req.sessionModel.get('passport-number-details') ? 'yes' : 'no',
+        passport_number: req.sessionModel.get('passport-number-details'),
+        has_brp_number: req.sessionModel.get('brp-details') ? 'yes' : 'no',
+        brp_number: req.sessionModel.get('brp-details'),
+        has_arc_number: req.sessionModel.get('arc-details') ? 'yes' : 'no',
+        arc_number: req.sessionModel.get('arc-details'),
+        who_are_you: req.sessionModel.get('who-are-you'),
+        is_legal_rep: req.sessionModel.get('legal-representative-name') ? 'yes' : 'no',
+        leg_rep_full_name: req.sessionModel.get('legal-representative-name'),
+        leg_rep_email_address: req.sessionModel.get('email'),
+        has_rep_tel_number: req.sessionModel.get('telephone').length > 0 ? 'yes' : 'no',
+        leg_rep_tel_number: req.sessionModel.get('telephone'),
+        has_leg_details: req.sessionModel.get('steps').includes('/legal-details') && req.sessionModel.get('oisc-sra-number') ? 'yes' : 'no',
+        OISC_SRA_number: req.sessionModel.get('oisc-sra-number') ?? '',
+        company_name: req.sessionModel.get('legal-company-name') ?? '',
+        leg_rep_address: req.sessionModel.get('legalAddressDetails') ?? '',
+        // details_updating: this.getUpdatedDetails(req), //THIS CAUSES ERROR
+        has_old_postcode: req.sessionModel.get('steps').includes('/old-address') ? 'yes' : 'no',
+        old_postcode: req.sessionModel.get('old-postcode') ?? '',
+        has_new_home_address: req.sessionModel.get('steps').includes('/home-address') ? 'yes' : 'no',
+        new_home_address: req.sessionModel.get('addressDetails') ?? '',
+        has_new_postal_address: req.sessionModel.get('steps').includes('/postal-address') ? 'yes' : 'no',
+        new_postal_address: req.sessionModel.get('postalAddressDetails') ?? '',
+        })
+      });
+
+      const trackedPageStartTime = Number(req.sessionModel.get('session.started.timestamp'));
+      const timeSpentOnForm = utilities.secondsBetween(trackedPageStartTime, new Date());
+
+      req.log('info', 'coa.submit_form.create_email_with_file_notify.successful');
+      req.log('info', `coa.submission.duration=[${timeSpentOnForm}] seconds`);
+    } catch (err) {
+      const error = _.get(err, 'response.data.errors[0]', err.message || err);
+      req.log('error', 'coa.submit_form.create_email_with_file_notify.error', error);
+      throw new Error(error);
+    }
+  }
+
+  async sendCaseworkerEmailNotification(req) {
+    const personalisations = this.notifyPersonalisations;
+
+    try {
+      if (notifyKey === 'USE_MOCK') {
+        req.log('warn', '*** Notify API Key set to USE_MOCK. Ensure disabled in production! ***');
+      }
+
+      await notifyClient.sendEmail(caseworkerConfirmationTemplateId, caseworkerEmail, {
+        personalisation: Object.assign({}, personalisations, {
           reference_number: req.sessionModel.get('uniqueRefNumber'),
           applying_for_whom: req.sessionModel.get('isApplicant') ? 'your' : req.sessionModel.get('nameWithPossession'),
           applicant_full_name: req.sessionModel.get('applicant-full-name'),
-          applicant_dob: req.sessionModel.get('applicant-dob'),
+          applicant_dob: req.sessionModel.get('applicant-dob'), 
           nationality: req.sessionModel.get('applicant-nationality'),
-          unique_reference_number: req.sessionModel.get('applicant-unique-number').length>0 ? req.sessionModel.get('applicant-unique-number') : 'Not Provided',
-          passport_number: req.sessionModel.get('passport-number-details').length>0 ? req.sessionModel.get('passport-number-details') : 'Not Provided',
-          brp_number: req.sessionModel.get('brp-details').length>0 ? req.sessionModel.get('brp-details') : 'Not Provided', 
-          arc_number: req.sessionModel.get('arc-details').length>0 ? req.sessionModel.get('arc-details') : 'Not Provided',
-          leg_rep_full_name: req.sessionModel.get('who-are-you'),
-          leg_rep_email_address: req.sessionModel.get('email'),
-          leg_rep_tel_number: req.sessionModel.get('telephone').length>0 ? req.sessionModel.get('telephone') : 'Not Provided',
-          OISC_SRA_number: req.sessionModel.get('oisc-sra-number'),
-          company_name: req.sessionModel.get('legal-company-name').length>0 ? req.sessionModel.get('legal-company-name') :'Not Provided',
-          leg_rep_address: req.sessionModel.get('legalAddressDetails').length>0 ? req.sessionModel.get('legalAddressDetails') : 'Not Provided',
-          details_updating: req.sessionModel.get('which-details-updating'),
-          old_postcode: req.sessionModel.get('old-postcode').length>0 ? req.sessionModel.get('old-postcode') : 'Not Provided',
-          new_home_address: req.sessionModel.get('addressDetails'),
-          new_postal_address: req.sessionModel.get('postalAddressDetails')
+          has_applicant_unique_number: req.sessionModel.get('applicant-unique-number').length > 0 ? 'yes' : 'no',
+          applicant_unique_number: req.sessionModel.get('applicant-unique-number'),
+          has_passport_number: req.sessionModel.get('passport-number-details') ? 'yes' : 'no',
+          passport_number: req.sessionModel.get('passport-number-details'),
+          has_brp_number: req.sessionModel.get('brp-details') ? 'yes' : 'no',
+          brp_number: req.sessionModel.get('brp-details'),
+          has_arc_number: req.sessionModel.get('arc-details') ? 'yes' : 'no',
+          arc_number: req.sessionModel.get('arc-details'),
+          is_legal_rep: req.sessionModel.get('legal-representative-name') ? 'yes' : 'no',
+          leg_rep_full_name: req.sessionModel.get('legal-representative-name'),
+          who_are_you: req.sessionModel.get('who-are-you'),
+          leg_rep_address: req.sessionModel.get('legalAddressDetails') ?? '',
+          has_rep_tel_number: req.sessionModel.get('telephone').length > 0 ? 'yes' : 'no',
+          leg_rep_tel_number: req.sessionModel.get('telephone'),
+          has_leg_details: req.sessionModel.get('steps').includes('/legal-details') && req.sessionModel.get('oisc-sra-number') ? 'yes' : 'no',
+          OISC_SRA_number: req.sessionModel.get('oisc-sra-number') ?? '',
+          company_name: req.sessionModel.get('legal-company-name') ?? '',
+          leg_rep_address: req.sessionModel.get('legalAddressDetails') ?? '',
+          // details_updating: this.getUpdatedDetails(req), //THIS CAUSES ERROR
+          has_old_postcode: req.sessionModel.get('steps').includes('/old-address') ? 'yes' : 'no',
+          old_postcode: req.sessionModel.get('old-postcode') ?? '',
+          has_new_home_address: req.sessionModel.get('steps').includes('/home-address') ? 'yes' : 'no',
+          new_home_address: req.sessionModel.get('addressDetails') ?? '',
+          has_new_postal_address: req.sessionModel.get('steps').includes('/postal-address') ? 'yes' : 'no',
+          new_postal_address: req.sessionModel.get('postalAddressDetails') ?? '',
         })
       });
 
@@ -59,7 +129,7 @@ module.exports = class SendEmailConfirmation {
   async send(req, res, locals) {
     try {
       await this.sendUserEmailNotification(req);
-      //await this.sendCaseworkerEmailNotification(req);
+      await this.sendCaseworkerEmailNotification(req);
 
       req.log('info', 'coa.form.submit_form.successful');
     } catch(e) {
