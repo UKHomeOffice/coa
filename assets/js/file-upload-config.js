e25f4779a6901c726f749ff24f8c40f6fa15c4f4
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2026-07-31 14:56:19 +0100

    COA-64 refactor file upload config

---
+++ assets/js/file-upload-config.js
@@ -0,0 +1,32 @@
+/**
+ * Shared file upload configuration used by both frontend and backend.
+ */
+
+module.exports = {
+    maxFileSizeInBytes: 25 * 1024 * 1024, // 25MiB in bytes
+    hostname: process.env.FILE_VAULT_URL,
+    allowedMimeTypes: [
+      'application/pdf',
+      'image/jpeg',
+      'image/jpg',
+      'image/png'
+    ],
+    documentCategories: {
+      'identity-documents': {
+        limit: 6,
+        limitValidationError: 'maxIdDocsUploads'
+      },
+      'home-address-documents': {
+        limit: 3,
+        limitValidationError: 'maxAddressDocsUploads'
+      },
+      'postal-address-documents': {
+        limit: 3,
+        limitValidationError: 'maxAddressDocsUploads'
+      },
+      'letter-of-authority': {
+        limit: 1,
+        limitValidationError: 'maxCertOfAuthorityUploads'
+      }
+    }
+}
