5c9fc371199a8aa5bb1ec8d600d1496f25f800ca
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2026-07-31 15:01:09 +0100

    COA-64 fix linting

--- assets/js/file-upload-config.js
+++ assets/js/file-upload-config.js
@@ -3,30 +3,30 @@
  */
 
 module.exports = {
-    maxFileSizeInBytes: 25 * 1024 * 1024, // 25MiB in bytes
-    hostname: process.env.FILE_VAULT_URL,
-    allowedMimeTypes: [
-      'application/pdf',
-      'image/jpeg',
-      'image/jpg',
-      'image/png'
-    ],
-    documentCategories: {
-      'identity-documents': {
-        limit: 6,
-        limitValidationError: 'maxIdDocsUploads'
-      },
-      'home-address-documents': {
-        limit: 3,
-        limitValidationError: 'maxAddressDocsUploads'
-      },
-      'postal-address-documents': {
-        limit: 3,
-        limitValidationError: 'maxAddressDocsUploads'
-      },
-      'letter-of-authority': {
-        limit: 1,
-        limitValidationError: 'maxCertOfAuthorityUploads'
-      }
+  maxFileSizeInBytes: 25 * 1024 * 1024, // 25MiB in bytes
+  hostname: process.env.FILE_VAULT_URL,
+  allowedMimeTypes: [
+    'application/pdf',
+    'image/jpeg',
+    'image/jpg',
+    'image/png'
+  ],
+  documentCategories: {
+    'identity-documents': {
+      limit: 6,
+      limitValidationError: 'maxIdDocsUploads'
+    },
+    'home-address-documents': {
+      limit: 3,
+      limitValidationError: 'maxAddressDocsUploads'
+    },
+    'postal-address-documents': {
+      limit: 3,
+      limitValidationError: 'maxAddressDocsUploads'
+    },
+    'letter-of-authority': {
+      limit: 1,
+      limitValidationError: 'maxCertOfAuthorityUploads'
     }
-}
+  }
+};
