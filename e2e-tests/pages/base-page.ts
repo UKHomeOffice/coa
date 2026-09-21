e8ef71cdd13f2933c31b04b28e1fde763509779d
GrahamOsifoHO <Graham.Osifo@digital.homeoffice.gov.uk>
2026-09-21 12:57:32 +0100

    COA-66 update message

--- e2e-tests/pages/base-page.ts
+++ e2e-tests/pages/base-page.ts
@@ -121,7 +121,7 @@ export class basePage {
 
     const dateParts = formattedDate.split('/');
     if (dateParts.length !== 3) {
-      throw new Error('Invalid date format. Expected format: dd/mm/yyyy');
+      throw new Error('Invalid date format. Expected date format to be: dd/mm/yyyy');
     }
 
     const [dayVal, monthVal, yearVal] = dateParts;
